7096e413ff35f83371167228e2b399657e6e1321
abhinav9444 <abhinavkrsingh9444@gmail.com>
2024-11-09 22:08:47 +0530

    new commit

---
+++ Lab-Codes/3.1c.c
@@ -0,0 +1,50 @@
+/*Ques 3.1 : WAP in TCP, client will send two numbers to server and server will calculate the sum. And send it back to the client, the client will display it.*/
+/*Client Side*/ 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int main()
+{
+	printf("\n**Client Interface\n");
+	int sockfd=socket(AF_INET,SOCK_STREAM,0);
+	printf("The socket value is : %d\n",sockfd);
+	
+	if(sockfd==-1)
+	{
+		perror("\nSocket creation failed !\n");
+		exit(0);
+	}
+	struct sockaddr_in server;
+	server.sin_port=htons(5000);
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=inet_addr("127.0.0.1");
+	
+	int c=connect(sockfd,(struct sockaddr*)&server,sizeof(server));
+	if(c==-1){
+	perror("\nConnection error !\n");
+	exit(0);
+	}
+	printf("\nSuccessfully connected !\n");
+	
+
+	
+	printf("\nEnter 2 numbers : ");
+	int a,b;
+	scanf("%d %d",&a,&b);
+	//Code for Sending 2 numbers
+	send(sockfd,&a,sizeof(int),0);
+	send(sockfd,&b,sizeof(int),0);
+	//Code for recieving the sum of sent 2 numbers
+	int data2;
+	recv(sockfd,&data2,sizeof(int),0);
+	//printing the received sum
+	printf("\nSum of the 2 number is : %d\n",data2);
+	return 0;
+}
---
+++ Lab-Codes/3.1s.c
@@ -0,0 +1,66 @@
+/*Ques 3.1 : WAP in TCP, client will send two numbers to server and server will calculate the sum. And send it back to the client, the client will display it.*/
+/*Server Side*/ 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+
+int main()
+{
+	printf("\n**Server Interface\n");
+	int sockfd=socket(AF_INET,SOCK_STREAM,0);
+	printf("The socket value is : %d\n",sockfd);
+	
+	if(sockfd==-1)
+	{
+		perror("\nSocket creation failed !\n");
+		exit(0);
+	}
+	struct sockaddr_in server,client;
+	server.sin_port=htons(5000);
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=inet_addr("127.0.0.1");
+	
+	int b=bind(sockfd,(struct sockaddr*)&server,sizeof(server));
+	printf("value of bind = %d\n",b);
+	
+	if(b==-1)
+	{
+		printf("\nError !\n");
+	}
+	int l=listen(sockfd,5);
+	if(l==-1)
+	{
+		perror("\nListen problem !\n");
+	}
+	else
+	{
+		printf("\nListen successful !\n");
+	}
+	
+	int size=sizeof(client);
+	int clientfd=accept(sockfd,(struct sockaddr*)&client,&size);
+	
+	if(clientfd==-1){
+	perror("\nNot connected to client !\n");
+	exit(0);
+	}
+	printf("Clientfd is : %d \n",clientfd);
+
+	//Code for Recieving two numbers
+	int a,b1;
+	recv(clientfd,&a,sizeof(int),0);
+	recv(clientfd,&b1,sizeof(int),0);
+	//Printing numbers received and calculating their sum
+ 	printf("Numbers Recieved from Client is : %d   %d\n",a,b1);
+	int sum=a+b1;
+	//Code for sending the sum
+	send(clientfd,&sum,sizeof(sum),0);
+	return 0;
+}
